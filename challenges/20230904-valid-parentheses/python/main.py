7046c9a3888e088a4606c5b4c0b9e4fc7f84490c
José Cabeda <jose.cabeda@fidel.uk>
2023-11-28 10:28:21 +0000

    update challenges

--- challenges/20230904-valid-parentheses/python/main.py
+++ challenges/20230904-valid-parentheses/python/main.py
@@ -4,16 +4,19 @@ import re
 
 # Given a string s containing just the characters '(', ')', '{', '}', '[' and ']', determine if the input string is valid. Must also accept incepted parentheses like "({})". It must always close the last opened bracket. Must match the entire string.
 
+# Runtime: 217ms (faster than 5.18%)
+# Memory: 16.2MB (beats 82.38%)
+
+
 class Solution:
     def isValid(self, s: str) -> bool:
 
         length = len(s)
         while length > 0:
-            shortened = re.sub(r'\(\)|\[\]|\{\}', '', s)
+            shortened = re.sub(r"\(\)|\[\]|\{\}", "", s)
             if shortened == s:
                 return False
             length = len(shortened)
             s = shortened
 
-        
         return True
