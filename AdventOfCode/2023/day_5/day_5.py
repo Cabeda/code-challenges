c54af8d5c917e85cd08c4d60d515fbb6edb38d39
José cabeda <jecabeda@gmail.com>
2023-12-09 12:22:45 +0000

    Move sol 5 to dict. Still brute forcing

--- AdventOfCode/2023/day_5/day_5.py
+++ AdventOfCode/2023/day_5/day_5.py
@@ -69,32 +69,25 @@ class Challenge():
     
     def conversion(self, seed, converters: list[Converter]):
         for converter in converters:
-            if seed >= converter.source and seed <= converter.source + converter.range:
+            if converter.source >= seed <= converter.source + converter.range:
                 return  converter.destination + seed - converter.source
         return seed
     
-    def seed_to_location(self, converters, seed):
-        soil_converters = [converter for converter in converters if converter.category == Category.soil]
-        soil = self.conversion(seed, soil_converters)
-        print(f"Converted seed {seed} to soil {soil}")
-        fertilizer_converters = [converter for converter in converters if converter.category == Category.fertilizer]
-        fertilizer = self.conversion(soil, fertilizer_converters)
-        print(f"Converted soil {soil} to fertilizer {fertilizer}")
-        water_converters = [converter for converter in converters if converter.category == Category.water]
-        water = self.conversion(fertilizer, water_converters)
-        print(f"Converted fertilizer {fertilizer} to water {water}")
-        light_converters = [converter for converter in converters if converter.category == Category.light]
-        light = self.conversion(water, light_converters)
-        print(f"Converted water {water} to light {light}")
-        temperature_converters = [converter for converter in converters if converter.category == Category.temperature]
-        temperature = self.conversion(light, temperature_converters)
-        print(f"Converted light {light} to temperature {temperature}")
-        humidity_converters = [converter for converter in converters if converter.category == Category.humidity]
-        humidity = self.conversion(temperature, humidity_converters)
-        print(f"Converted temperature {temperature} to humidity {humidity}")
-        location_converters = [converter for converter in converters if converter.category == Category.location]
-        location = self.conversion(humidity, location_converters)
-        print(f"Converted humidity {humidity} to location {location}")
+    def seed_to_location(self, converters: dict[Category, list], seed):
+        soil = self.conversion(seed, converters[Category.soil])
+        # print(f"Converted seed {seed} to soil {soil}")
+        fertilizer = self.conversion(soil, converters[Category.fertilizer])
+        # print(f"Converted soil {soil} to fertilizer {fertilizer}")
+        water = self.conversion(fertilizer, converters[Category.water])
+        # print(f"Converted fertilizer {fertilizer} to water {water}")
+        light = self.conversion(water, converters[Category.light])
+        # print(f"Converted water {water} to light {light}")
+        temperature = self.conversion(light, converters[Category.temperature])
+        # print(f"Converted light {light} to temperature {temperature}")
+        humidity = self.conversion(temperature, converters[Category.humidity])
+        # print(f"Converted temperature {temperature} to humidity {humidity}")
+        location = self.conversion(humidity, converters[Category.location])
+        # print(f"Converted humidity {humidity} to location {location}")
 
         return location
 
@@ -102,11 +95,16 @@ class Challenge():
     def challenge_1(self, mapper: Mapper):
         min = mapper.seeds[0]
 
+        converters_dict: dict[Category, list] = {category: [] for category in Category}
+        for converter in mapper.converters:
+            converters_dict[converter.category].append(converter)
+
         for seed in mapper.seeds:
-            location = self.seed_to_location(mapper.converters, seed)
+            location = self.seed_to_location(converters_dict, seed)
 
             if location < min:
                 min = location
+                print(f"New min: {min}")
 
         print(min)
 
@@ -114,13 +112,18 @@ class Challenge():
     def challenge_2(self, mapper: Mapper):
         min = None
         location = None
+
+        converters_dict: dict[Category, list] = {category: [] for category in Category}
+        for converter in mapper.converters:
+            converters_dict[converter.category].append(converter)
     
         for i in range(0, len(mapper.seeds), 2):
             print(f"Start range {mapper.seeds[i]}:{mapper.seeds[i] + mapper.seeds[i+1]}")
             for seed in  range(mapper.seeds[i], mapper.seeds[i] + mapper.seeds[i+1]):
-                location = self.seed_to_location(mapper.converters, seed)
+                location = self.seed_to_location(converters_dict, seed)
                 if not min or location < min:
                     min = location
+                    print(f"New min: {min}")
 
         print(min)
         
