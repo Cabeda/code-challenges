b6922f2d108afdcf24dea5669e2712f5d4834162
José Cabeda <jose.cabeda@fidel.uk>
2023-12-12 22:52:24 +0000

    Git remove files on gitignore

--- AdventOfCode/2022/threesum/target/debug/deps/threesum-ec7bae7e84e590f7.d
+++
@@ -1,5 +0,0 @@
-/Users/josecabeda/Git/Pensamentos/Conhecimento/AdventOfCode/2022/threesum/target/debug/deps/threesum-ec7bae7e84e590f7.rmeta: src/main.rs
-
-/Users/josecabeda/Git/Pensamentos/Conhecimento/AdventOfCode/2022/threesum/target/debug/deps/threesum-ec7bae7e84e590f7.d: src/main.rs
-
-src/main.rs:
